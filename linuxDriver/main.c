c59a025b1be39f7842cf2bde6b4854d6a8d86593
sean <seanep94@gmail.com>
2024-03-11 20:45:16 -0700

    Chromebook cant run linux drivers(Might need more configuring but out of my knowledge window rn)

---
+++ linuxDriver/main.c
@@ -0,0 +1,14 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+
+static int __init ModuleInit(void) {
+    printk("Hello Kernel!\n");
+    return 0;
+}
+
+static void __exit ModuleExit(void) {
+    printk("Goodbye Kernel!\n");
+}
